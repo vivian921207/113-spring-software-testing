82f88a77468348e4b6f4a20a0abfda11b0ae9c11
vivian921207 <lf2.freeze@gmail.com>
2025-04-26 05:56:24 +0800

    update something

---
+++ lab5/antiasan.c
@@ -0,0 +1,18 @@
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+
+void antiasan(unsigned long addr) {
+    const unsigned long kShadowOffset = 0x7fff8000;
+
+    unsigned long shadow_start = (addr + 0x87 >> 3) + kShadowOffset;
+    unsigned long shadow_end   = ((addr + 0x87 + 0x58) >> 3) + kShadowOffset;
+
+
+    if (1) {
+        *(char *)shadow_end = 0x00;
+        shadow_end   = ((addr + 0x87 + 0x60) >> 3) + kShadowOffset;
+        *(char *)shadow_end = 0x00;
+        }
+
+}
